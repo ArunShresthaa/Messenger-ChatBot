9922aff98fd9173af2e77b78e66f3597af16575f
Arun Shrestha <arunstha547@gmail.com>
2023-07-16 22:44:56 +0545

    Upload

--- autoreply.py
+++ autoreply.py
@@ -1,27 +1,31 @@
 import fbchat
+import requests
 import re
 from fbchat import log, Client
 from fbchat.models import *
+
+def session_factory(user_agent=None):
+    session = requests.session()
+    session.headers["Referer"] = "https://www.facebook.com"
+    session.headers["Accept"] = "text/html"
+
+    # TODO: Deprecate setting the user agent manually
+    return session
+
+fbchat._state.session_factory = session_factory
 fbchat._state.FB_DTSG_REGEX = re.compile(r'"token":"(.*?)"')
 # Subclass the `Client` class to create your own client
 class AutoReplyClient(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(thread_id, message_object.uid)  # Mark message as delivered
-        self.markAsRead(thread_id)  # Mark message as read
+        self.markAsDelivered(thread_id, message_object.uid)  
+        self.markAsRead(thread_id)  
 
-        # log.info("Message from {} in {} ({}): {}".format(author_id, thread_id, thread_type.name, message_object.text))
-
-        # Check if the message is from a user and not the client itself
         if author_id != self.uid:
             reply_text = "Thank you for your message!\nI am busy right now.\nWill reply ASAP. This is an automated reply."
             self.send(Message(text=reply_text), thread_id=thread_id, thread_type=thread_type)
 
-        # Continue listening for messages
-        # self.listen()
-
-
-username = ''
-password = ''
+username = '9745979351'
+password = 'arun@159'
 # Create an instance of your custom client
 client = AutoReplyClient(username,password)
 
